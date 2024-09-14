5db7dbe5af8f1768307ef4bb1f5007c5f84a95db
unknown <nelioramos642@gmail.com>
2024-09-14 10:43:03 -0300

    oi

---
+++ git23/calculadora basica.py
@@ -0,0 +1,14 @@
+import string
+
+num1 = float(input ("Digite o primeiro numero:"))
+num2 = float(input("Digite o segundo numero:"))
+
+soma = num1 + num2
+subtração = num1 - num2
+divisão = num1 / num2
+multiplicação = num1 * num2
+
+print("Soma:", soma)
+print("Subtracao:", subtração)
+print("Divisao:", divisão)
+print("Multiplicacao:", multiplicação)
---
+++ git23/teste de idade.py
@@ -0,0 +1,9 @@
+import string
+
+idade = int(input("Digite sua idade:"))
+
+if idade < 10:
+    print("nossa voce e novo")
+
+else:
+    print("nossa voce ta velho")
